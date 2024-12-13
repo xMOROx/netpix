ecbf2bdc1ab972334b816fd36e0ee15bc24eb971
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-12-13 14:13:14 +0100

    refactor: concise server impl

--- src/server.rs
+++ src/server.rs
@@ -4,50 +4,25 @@ mod constants;
 mod handler;
 
 use crate::sniffer::Sniffer;
-use constants::{CLIENT_MESSAGE_INTERVAL_MS, WEBSOCKET_PATH};
 use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::time::Duration;
-use tokio::time::interval;
 use warp::Filter;
 
+use netpix_macros::{
+    run_server, setup_clients, setup_packet_handlers, setup_routes, spawn_message_sender,
+};
+
 pub async fn run(sniffers: HashMap<String, Sniffer>, addr: SocketAddr) {
-    let clients = client::new_clients();
-    let source_to_packets = handler::setup_packet_handlers(sniffers, clients.clone()).await;
+    let clients = setup_clients!();
+    let source_to_packets = setup_packet_handlers!((sniffers, clients));
+    let sender_clients = clients.clone();
 
-    let clients_clone = clients.clone();
-    let clients_filter = warp::any().map(move || clients_clone.clone());
-    let source_to_packets_filter = warp::any().map(move || source_to_packets.clone());
-    let ws = warp::path(WEBSOCKET_PATH)
-        .and(warp::ws())
-        .and(clients_filter)
-        .and(source_to_packets_filter)
-        .map(|ws: warp::ws::Ws, clients_cl, source_to_packets_cl| {
-            ws.on_upgrade(move |socket| {
-                client::handle_connection(socket, clients_cl, source_to_packets_cl)
-            })
-        });
+    let routes = setup_routes!((clients, source_to_packets));
 
-    let index_html = warp::path::end().and_then(asset::serve_index);
-    let other = warp::path::tail().and_then(asset::serve);
+    spawn_message_sender!((
+        sender_clients,
+        crate::server::constants::CLIENT_MESSAGE_INTERVAL_MS
+    ));
 
-    let routes = ws.or(index_html).or(other);
-
-    let clients_for_sender = clients.clone();
-    tokio::spawn(async move {
-        let mut ticker = interval(Duration::from_millis(CLIENT_MESSAGE_INTERVAL_MS));
-        loop {
-            ticker.tick().await;
-            let mut clients = clients_for_sender.write().await;
-
-            for client in clients.values_mut() {
-                if let Some(msg) = client.queue.pop_front() {
-                    client.sender.send(msg).ok();
-                }
-            }
-        }
-    });
-
-    println!("Netpix running on http://{}/", addr);
-    warp::serve(routes).try_bind(addr).await;
+    run_server!((routes, addr));
 }
