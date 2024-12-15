8a621a981647dfcf04a58c9d39fdf3a5072a43a3
Patryk Zajdel <57092803+xMOROx@users.noreply.github.com>
2024-12-15 10:31:09 +0100

    chore: change number of messages per second

--- src/server/constants.rs
+++ src/server/constants.rs
@@ -1,3 +1,3 @@
 pub const WEBSOCKET_PATH: &str = "ws";
 pub const PACKET_BUFFER_SIZE: usize = 32_768;
-pub const CLIENT_MESSAGE_INTERVAL_MS: u64 = 10; // ~ 150 messages per second
+pub const CLIENT_MESSAGE_INTERVAL_MS: u64 = 5; // ~ 300 messages per second
