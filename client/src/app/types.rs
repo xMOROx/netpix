9018b2c3010a5f33720fe5e725e1cd6128817e23
Bartlomiej Stylski <stylub@github.com>
2025-12-05 15:43:21 +0100

    reverted accidentally deleted line of code

--- client/src/app/types.rs
+++ client/src/app/types.rs
@@ -77,6 +77,7 @@ impl App {
         table_registry.register::<MpegTsPacketsTable>(streams.clone(), ws_sender.clone());
         table_registry.register::<MpegTsStreamsTable>(streams.clone(), ws_sender.clone());
         table_registry.register::<MpegTsInformationTable>(streams.clone(), ws_sender.clone());
+        table_registry.register::<StunPacketsTable>(streams.clone(), ws_sender.clone());
         plot_registry.register::<RtpStreamsPlot>(streams.clone(), ws_sender.clone());
 
         let (tab, selected_source) = get_initial_state(cc);
